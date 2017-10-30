c755b61a9b29a2a981ac2d04850650e595854236
Johan Dahlin <johan.dahlin@newcastle.edu.au>
2017-10-31 08:33:29 +1100

    updated scripts for examples in paper.
    Former-commit-id: 786c33fc1182e61c9ef9976833f426684b7d2f28

--- example2-lgss-particles.py
+++ example2-lgss-particles.py
@@ -1,10 +1,10 @@
 import numpy as np
-import scripts.mh2 as mh
-import scripts.qmh as qmh
+import scripts.mh2_lgss as mh
+import scripts.qmh_lgss as qmh
 
 mh_settings = {'no_iters': 5000,
                'no_burnin_iters': 1000,
-               'step_size': 0.8,
+               'step_size': 0.5,
                'base_hessian': np.eye(3) * 0.05**2,
                'initial_params': (0.0, 0.1, 0.2),
                'hessian_correction_verbose': True,
@@ -16,36 +16,37 @@ mh_settings = {'no_iters': 5000,
                'qn_memory_length': 20
                }
 
-sim_name = 'mh2'
-mh.run(new_settings=mh_settings, sim_name=sim_name)
+pf_settings = {'resampling_method': 'systematic',
+               'no_particles': 1000,
+               'estimate_gradient': True,
+               'estimate_hessian_segalweinstein': True,
+               'fixed_lag': 10,
+               'generate_initial_state': True
+              }
+
+sim_name = 'example2_mh2'
+mh.run(new_mh_settings=mh_settings, sim_name=sim_name)
 
 mh_settings.update({'qn_strategy': 'bfgs'})
-sim_name = 'qmh_bfgs'
+sim_name = 'example2_qmh_bfgs'
 sim_desc = ('Damped BFGS for estimating Hessian. Scaling the initial Hessian ',
            'such that the gradient gives a step of 0.01. Non-PD estimates ',
            'are replaced with an empirical approximation of the Hessian.')
-qmh.run(new_settings=mh_settings, sim_name=sim_name, sim_desc=sim_desc)
+qmh.run(new_mh_settings=mh_settings, new_pf_settings=pf_settings,
+        smoothing_method="particle", sim_name=sim_name, sim_desc=sim_desc)
 
 mh_settings.update({'qn_strategy': 'sr1', 'hessian_correction': 'flip'})
-sim_name = 'qmh_sr1_flip'
+sim_name = 'example2_qmh_sr1_flip'
 sim_desc = ('SR1 for estimating Hessian. Scaling the initial Hessian ',
            'such that the gradient gives a step of 0.01. Non-PD estimates ',
            'are corrected by flipping negative eigenvalues.')
-qmh.run(new_settings=mh_settings, sim_name=sim_name, sim_desc=sim_desc)
+qmh.run(new_mh_settings=mh_settings, new_pf_settings=pf_settings,
+        smoothing_method="particle", sim_name=sim_name, sim_desc=sim_desc)
 
 mh_settings.update({'qn_strategy': 'sr1', 'hessian_correction': 'replace'})
-sim_name = 'qmh_sr1_hyb'
+sim_name = 'example2_qmh_sr1_hyb'
 sim_desc = ('SR1 for estimating Hessian. Scaling the initial Hessian ',
            'such that the gradient gives a step of 0.01. Non-PD estimates ',
            'are replaced with an empirical approximation of the Hessian.')
-qmh.run(new_settings=mh_settings, sim_name=sim_name, sim_desc=sim_desc)
-
-mh_settings.update({'qn_strategy': 'sr1', 'hessian_correction': 'replace',
-                    'qn_sr1_safe_parameterisation': True,
-                    'step_size': 1.0})
-sim_name = 'qmh_sr1_safe'
-sim_desc = ('SR1 for estimating Hessian. Scaling the initial Hessian ',
-           'such that the gradient gives a step of 0.01. Non-PD estimates ',
-           'are replaced with an empirical approximation of the Hessian. ',
-           'Safe parameterisation is used.')
-#qmh.run(new_settings=mh_settings, sim_name=sim_name, sim_desc=sim_desc)
+qmh.run(new_mh_settings=mh_settings, new_pf_settings=pf_settings,
+        smoothing_method="particle", sim_name=sim_name, sim_desc=sim_desc)
--- scripts/mh2_lgss.py
+++ scripts/mh2_lgss.py
@@ -3,9 +3,11 @@ import matplotlib.pylab as plt
 
 from models.linear_gaussian_model import SystemModel
 from state.kalman_methods.main import KalmanMethods
+from state.particle_methods.main import ParticleMethods
 from parameter.mcmc.metropolis_hastings import MetropolisHastings
 
-def run(new_settings=None, sim_name='test', sim_desc='...'):
+def run(new_mh_settings=None, new_kf_settings=None, new_pf_settings=None,
+        smoothing_method="kalman", sim_name='test', sim_desc='...'):
     # System model
     sys_model = SystemModel()
     sys_model.params['mu'] = 0.20
@@ -24,7 +26,21 @@ def run(new_settings=None, sim_name='test', sim_desc='...'):
 
     # Kalman filter and smoother
     kf_settings = {'initial_state': sys_model.initial_state}
+    if new_kf_settings:
+        kf_settings.update(new_kf_settings)
     kf = KalmanMethods(kf_settings)
+
+    # Particle filter and smoother
+    particle_settings = {'resampling_method': 'systematic',
+                         'no_particles': 1000,
+                         'estimate_gradient': True,
+                         'estimate_hessian_segalweinstein': True,
+                         'fixed_lag': 10,
+                         'generate_initial_state': True
+                        }
+    if new_pf_settings:
+        particle_settings.update(new_pf_settings)
+    pf = ParticleMethods(particle_settings)
 
     # Metropolis-Hastings
     mh_settings = {'no_iters': 1000,
@@ -37,8 +53,15 @@ def run(new_settings=None, sim_name='test', sim_desc='...'):
                    'hessian_correction_verbose': True
                    }
 
-    if new_settings:
-        mh_settings.update(new_settings)
+    if new_mh_settings:
+        mh_settings.update(new_mh_settings)
     mh = MetropolisHastings(sys_model, 'mh2', mh_settings)
-    mh.run(kf)
+
+    if smoothing_method is 'kalman':
+        mh.run(kf)
+    elif smoothing_method is 'particle':
+        mh.run(pf)
+    else:
+        raise NameError("Unknown smoothing method selected")
+
     mh.save_to_file(output_path='results', sim_name=sim_name, sim_desc=sim_desc)
