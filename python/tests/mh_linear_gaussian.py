73dfe41e670acc8bc4cb83198929a03e7e75d9c7
Johan Dahlin <johan.dahlin@newcastle.edu.au>
2017-11-03 16:30:01 +1100

    changed step sizes in mh test.
    Former-commit-id: e84b6b0732155f47c38ffd0ef40b8465b948ed55

--- python/tests/mh_linear_gaussian.py
+++ python/tests/mh_linear_gaussian.py
@@ -57,6 +57,7 @@ def run(filter_method='kalman', alg_type='mh0', plotting=True):
             mh.settings['step_size'] = 0.8
         else:
             raise NameError("Unknown alg_type (mh0/mh1/mh2/qmh).")
+        mh.settings['step_size'] = 0.5 * mh.settings['step_size']
         mh.run(pf)
     else:
         raise NameError("Unknown filter_method (kalman/particle).")
