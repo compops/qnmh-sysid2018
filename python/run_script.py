8e6fc88f99943c4fa55ba893a22bad87eeaa5797
Johan Dahlin <johan.dahlin@newcastle.edu.au>
2018-06-07 09:42:31 +1000

    updated run scripts to allow for short runs and only a single experiment.

--- python/run_script.py
+++ python/run_script.py
@@ -5,15 +5,29 @@ import scripts.example2_lgss_particles as example2
 import scripts.example3_stochastic_volatility as example3
 
 if len(sys.argv) > 1:
-    if int(sys.argv[1]) == 1:
-        for i in range(25):
-            example1.main(seed_offset=i)
-    elif int(sys.argv[1]) == 2:
-        for i in range(25):
-            example2.main(seed_offset=i)
-    elif int(sys.argv[1]) == 3:
-        example3.main(seed_offset=0)
+    if (len(sys.argv) > 2) and int(sys.argv[2]) == 1:
+        print("Running full experiment (25 Monte Carlo runs).")
+        print("This will probably take a few hours.")
+        NO_ITERS = 25
     else:
-        raise NameError("Unknown example to run...")
+        print("Running reduced experiment (1 Monte Carlo run).")
+        NO_ITERS = 1
+
+    if int(sys.argv[1]) == 1:
+        print("Running first example.")
+        for i in range(NO_ITERS):
+            example1.main(seed_offset=i)
+
+    elif int(sys.argv[1]) == 2:
+        print("Running second example.")
+        for i in range(NO_ITERS):
+            example2.main(seed_offset=i)
+
+    elif int(sys.argv[1]) == 3:
+        print("Running third example.")
+        example3.main(seed_offset=0)
+
+    else:
+        raise NameError("Unknown example.")
 else:
-    raise NameError("Need to supply an argument to function call (0, 1, 2, 3) corresponding to the numerical illustration to run.")
+    raise NameError("Need provide the experiment to run.")
--- python/run_script.sh
+++ python/run_script.sh
@@ -2,14 +2,5 @@
 python setup.py build_ext --inplace
 wait
 
-python run_script.py 1
+python run_script.py $EXPERIMENT $FULLRUN
 wait
-
-python run_script.py 2
-wait
-
-python run_script.py 3
-wait
-
-# Compress the results into one file
-tar -zcvf /app/qnmh-sysid2018-results.tgz /app/results/*
